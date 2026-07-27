f2520697f5c610b6f8cdb1320546613eae856e83
Tim <tim@timakai.me>
2026-07-27 17:37:12 +0200

    build: Add AOT cache support for runServer
    Signed-off-by: Tim <tim@timakai.me>

--- buildSrc/src/main/kotlin/project.paper-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/project.paper-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import xyz.jpenilla.runpaper.task.RunServer
+
 plugins {
     java
     alias(libs.plugins.runPaper)
@@ -12,4 +14,22 @@ repositories {
 
 dependencies {
     compileOnly(libs.paperApi)
+}
+
+tasks {
+    runServer {
+        jvmArgs("-XX:AOTCache=server.aot")
+    }
+    register<RunServer>("recordServerAOTCache") {
+        version = runServer.get().version
+        group = "run paper"
+        description = "Record (1) Ahead Of Time Cache for the runServer task"
+        jvmArgs("-XX:AOTMode=record", "-XX:AOTConfiguration=server.aotconf")
+    }
+    register<RunServer>("createServerAOTCache") {
+        version = runServer.get().version
+        group = "run paper"
+        description = "Create (2) Ahead Of Time Cache for the runServer task"
+        jvmArgs("-XX:AOTMode=create", "-XX:AOTConfiguration=server.aotconf", "-XX:AOTCache=server.aot")
+    }
 }
