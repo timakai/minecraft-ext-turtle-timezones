884eb18f18f65a58fba05e5aee30fd02702a2cba
Tim <tim@timakai.me>
2026-07-31 20:14:57 +0200

    paper: Use lombok for plugin constructor

--- paper/src/main/java/me/timakai/minecraft/ext/turtle/timezones/Main.java
+++ paper/src/main/java/me/timakai/minecraft/ext/turtle/timezones/Main.java
@@ -1,13 +1,11 @@
 package me.timakai.minecraft.ext.turtle.timezones;
 
+import lombok.*;
 import net.kyori.adventure.text.logger.slf4j.*;
 import org.bukkit.plugin.java.*;
 
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public final class Main extends JavaPlugin {
 
     private final ComponentLogger logger;
-
-    Main(ComponentLogger logger) {
-        this.logger = logger;
-    }
 }
