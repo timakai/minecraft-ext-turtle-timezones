f3d50c203682da8271a61d7d1c4c35088d40f5d4
Tim <tim@timakai.me>
2026-08-06 16:41:22 +0200

    paper: Create Jdbi configuration class

---
+++ paper/src/main/java/me/timakai/minecraft/ext/turtle/timezones/core/JdbiConfig.java
@@ -0,0 +1,17 @@
+package me.timakai.minecraft.ext.turtle.timezones.core;
+
+import org.jdbi.v3.core.*;
+import org.jdbi.v3.sqlobject.*;
+import org.jdbi.v3.sqlobject.locator.*;
+
+public interface JdbiConfig {
+
+    static void applyTo(Jdbi jdbi) {
+        jdbi.installPlugin(new SqlObjectPlugin());
+        jdbi.configure(SqlObjects.class, JdbiConfig::configureSqlObjects);
+    }
+
+    private static void configureSqlObjects(SqlObjects config) {
+        config.setSqlLocator(new SqlObjectClasspathSqlLocator());
+    }
+}
