ef7206fe1c6d748af77537f227c1ebaf03bc1a3b
Tim <tim@timakai.me>
2026-08-06 17:19:08 +0200

    paper: Install JDBI SQlite plugin

--- paper/src/main/java/me/timakai/minecraft/ext/turtle/timezones/core/JdbiConfig.java
+++ paper/src/main/java/me/timakai/minecraft/ext/turtle/timezones/core/JdbiConfig.java
@@ -1,12 +1,14 @@
 package me.timakai.minecraft.ext.turtle.timezones.core;
 
 import org.jdbi.v3.core.*;
+import org.jdbi.v3.sqlite3.*;
 import org.jdbi.v3.sqlobject.*;
 import org.jdbi.v3.sqlobject.locator.*;
 
 public interface JdbiConfig {
 
     static void applyTo(Jdbi jdbi) {
+        jdbi.installPlugin(new SQLitePlugin());
         jdbi.installPlugin(new SqlObjectPlugin());
         jdbi.configure(SqlObjects.class, JdbiConfig::configureSqlObjects);
     }
